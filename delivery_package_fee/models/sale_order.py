isabella232/delivery-carrier

delivery_package_fee: fix copy w/ no order lines
'order_line' can be empty when SO is copied
from direct call to copy with False as default.

delivery_package_fee/models/sale_order.py
@@ -93,7 +93,7 @@ class SaleOrder(models.Model):
         new_result = []
         for values in result:
             # "sale" module sets this key
-            if "order_line" in values:
+            if "order_line" in values and values["order_lne"]:
                 # remove package fee lines
                 order_lines = [
                     line

t-core-one/oca-delivery-carrier

delivery_package_fee/models/sale_order.py
@@ -93,7 +93,7 @@ class SaleOrder(models.Model):
         new_result = []
         for values in result:
             # "sale" module sets this key
-            if "order_line" in values:
+            if "order_line" in values and values["order_line"]:
                 # remove package fee lines
                 order_lines = [
                     line